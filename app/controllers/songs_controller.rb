2f3a3587e59c07bcbe28e7f0b0d89e8c165f06a6
Sergey Filippov <sergey.filippov.89@inbox.ru>
2020-08-11 11:51:15 +0800

    wrote sudo code for songs#create

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,24 @@ class SongsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:create, :display_global]
 
   def create
+    # receive the link
+    # understand what app it belongs to
+    # check if it is in the DB
 
+    # if Spotify
+
+    # regex to cut out the id
+    # make a query to Spotify fot track obj
+    # get artist and title
+    # make queries to NetEase and QQ in order to get a link
+    # create a song with all the attributes from 3 apps track objs
+    # make up a msg
+
+
+    # if Ease Net
+
+
+    # if QQ
   end
 
   def display_global
