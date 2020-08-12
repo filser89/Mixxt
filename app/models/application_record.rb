ea59f4a4aaadb50979e4be9fd897e96268a192f0
Sergey Filippov <sergey.filippov.89@inbox.ru>
2020-08-12 18:07:16 +0800

    now the message is generated if the song is already in our db

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
+
   self.abstract_class = true
+
+  include ApiCall
 end
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Mixxt
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.autoload_paths += Dir[Rails.root.join("app", "models", "general_modules")]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
