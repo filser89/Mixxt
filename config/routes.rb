656f99f082a341bfc1481eb37428eca4ceb39a97
helen ling <hling.mim2014@london.edu>
2020-08-14 14:40:10 +0800

    popup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :songs, only: [:create]
+  get 'fetch_msg', to: 'songs#fetch_msg'
   get 'global', to: 'songs#display_global'
   get 'favorites', to: 'songs#display_favorites'
   get 'history', to: 'songs#display_history'
