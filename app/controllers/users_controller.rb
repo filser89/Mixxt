7aa77283c10543fec2a91f506b2155a355720a3e
Sergey Filippov <sergey.filippov.89@inbox.ru>
2020-08-18 12:58:36 +0800

    created nessary actions in the users controller and very basic views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require[:user].permit(:first_name, :last_name, :audd_key)
+    params.require(:user).permit(:first_name, :last_name, :audd_key)
   end
 end
