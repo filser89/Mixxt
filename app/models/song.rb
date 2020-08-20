2bdd048006f132e75c984acbf9a2e3126b844204
Sergey Filippov <sergey.filippov.89@inbox.ru>
2020-08-20 16:21:54 +0800

    created eccapes in case if the song is not found on one of the apps, created a view for song_recognition

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,10 +5,14 @@ class Song < ApplicationRecord
   has_many :histories, dependent: :destroy
 
   def generate_msg
-    {
-      spotify: "#{song_details.find_by(app: "spotify").url}",
-      net_ease: "#{song_details.find_by(app: "net_ease").url}",
-      qq: "#{song_details.find_by(app: "qq").url}"
-    }
+    msg = {}
+    spotify = song_details.find_by(app: "spotify")
+    msg[:spotify] = spotify ? spotify.url : "Could not find this song on Spotify."
+    net_ease = song_details.find_by(app: "net_ease")
+    msg[:net_ease] = net_ease ? net_ease.url : "Could not find this song on NetEase Music"
+    qq = song_details.find_by(app: "qq")
+    msg[:qq] = qq ? qq.url : "Could not find this song on QQ Music"
+    p msg
+    msg
   end
 end
